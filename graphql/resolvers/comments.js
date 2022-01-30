dd64532c644dc1ef7fcb1a25cb0a5b2d1e881af3
chinmay-prajapat <prajapatchinmay@gmail.com>
2022-01-30 22:46:56 +0530

    creating and deleting comment

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -2,6 +2,7 @@ const { AuthenticationError, UserInputError } = require("apollo-server");
 
 const checkAuth = require("../../utils/check_auth");
 const Post = require("../../models/Post");
+const check_auth = require("../../utils/check_auth");
 
 module.exports = {
   Mutation: {
@@ -45,6 +46,24 @@ module.exports = {
       } else {
         throw new UserInputError("Post not found");
       }
+    },
+
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          post.likes = post.likes.filter((like) => like.username !== username);
+          await post.save();
+        } else {
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post not found");
     },
   },
 };
